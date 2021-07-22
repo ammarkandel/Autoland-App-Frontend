cdefa9118261e3f7349a89b221a3fc29f9554839
ammarkandel <merom8703@gmail.com>
2021-07-22 12:38:16 +0200

    Fix linter warnings

--- src/__test__/SignupForm.test.js
+++ src/__test__/SignupForm.test.js
@@ -1,58 +1,58 @@
-import { render, fireEvent, wait } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../store/index';
 import SignupForm from '../containers/SignupForm';
 
 test('Test form validation if user add invalid inputs', async () => {
-    const { getByLabelText, getByRole, getByText } = render(
-    <Provider store={store}>
-      <SignupForm />
-    </Provider>
-    );
+  const { getByLabelText, getByText } = render(
+  <Provider store={store}>
+    <SignupForm />
+  </Provider>
+  );
 
-    const passwordInput = getByLabelText(/Password:/i);
-    const emailInput = getByLabelText(/E-Mail/i);
-    const submitBtn = getByText(/Submit/i);
+  const passwordInput = getByLabelText(/Password:/i);
+  const emailInput = getByLabelText(/E-Mail/i);
+  const submitBtn = getByText(/Submit/i);
 
-    fireEvent.change(passwordInput, { 'target': { 'value': '1678' } });
-    fireEvent.change(emailInput, { 'target': { 'value': 'test123gmailcom' } });
-    fireEvent.click(submitBtn);
-    expect(submitBtn).toHaveAttribute('disabled');
+  fireEvent.change(passwordInput, { 'target': { 'value': '1678' } });
+  fireEvent.change(emailInput, { 'target': { 'value': 'test123gmailcom' } });
+  fireEvent.click(submitBtn);
+  expect(submitBtn).toHaveAttribute('disabled');
 })
 
 test('Test form validation if username is empty', async () => {
-    const { getByLabelText, getByRole, getByText } = render(
-    <Provider store={store}>
-      <SignupForm />
-    </Provider>
-    );
+  const { getByLabelText, getByText } = render(
+  <Provider store={store}>
+    <SignupForm />
+  </Provider>,
+  );
 
-    const usernameInput = getByLabelText(/Username:/i);
-    const passwordInput = getByLabelText(/Password:/i);
-    const emailInput = getByLabelText(/E-Mail/i);
-    const submitBtn = getByText(/Submit/i);
+  const usernameInput = getByLabelText(/Username:/i);
+  const passwordInput = getByLabelText(/Password:/i);
+  const emailInput = getByLabelText(/E-Mail/i);
+  const submitBtn = getByText(/Submit/i);
 
-    fireEvent.change(usernameInput, { 'target': { 'value': '' } });
-    fireEvent.change(passwordInput, { 'target': { 'value': '12345678' } });
-    fireEvent.change(emailInput, { 'target': { 'value': 'test123@gmail.com' } });
-    fireEvent.click(submitBtn);
-    expect(submitBtn).toHaveAttribute('disabled');
+  fireEvent.change(usernameInput, { 'target': { 'value': '' } });
+  fireEvent.change(passwordInput, { 'target': { 'value': '12345678' } });
+  fireEvent.change(emailInput, { 'target': { 'value': 'test123@gmail.com' } });
+  fireEvent.click(submitBtn);
+  expect(submitBtn).toHaveAttribute('disabled');
 })
 
 test('Test form validation if user add valid inputs', async () => {
-    const { getByLabelText, getByRole, getByText } = render(
-    <Provider store={store}>
-      <SignupForm />
-    </Provider>
-    );
-    const usernameInput = getByLabelText(/Username:/i);
-    const passwordInput = getByLabelText(/Password:/i);
-    const emailInput = getByLabelText(/E-Mail/i);
-    const submitBtn = getByText(/Submit/i);
+  const { getByLabelText, getByText } = render(
+  <Provider store={store}>
+    <SignupForm />
+  </Provider>,
+  );
+  const usernameInput = getByLabelText(/Username:/i);
+  const passwordInput = getByLabelText(/Password:/i);
+  const emailInput = getByLabelText(/E-Mail/i);
+  const submitBtn = getByText(/Submit/i);
 
-    fireEvent.change(usernameInput, { 'target': { 'value': 'test name' } });
-    fireEvent.change(passwordInput, { 'target': { 'value': '12345678' } });
-    fireEvent.change(emailInput, { 'target': { 'value': 'test123@gmail.com' } });
-    fireEvent.click(submitBtn);
-    expect(submitBtn).not.toHaveAttribute('disabled=""');
+  fireEvent.change(usernameInput, { 'target': { 'value': 'test name' } });
+  fireEvent.change(passwordInput, { 'target': { 'value': '12345678' } });
+  fireEvent.change(emailInput, { 'target': { 'value': 'test123@gmail.com' } });
+  fireEvent.click(submitBtn);
+  expect(submitBtn).not.toHaveAttribute('disabled=""');
 })
